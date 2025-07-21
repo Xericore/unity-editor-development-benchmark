92cd05ac1742bfcad7a8b710a1629c4d505136b0
Xericore <xericore@gmail.com>
2025-07-21 10:11:57 +0200

    Fixes BuildUserWaitTimeTracker.

--- Assets/Scripts/Editor/UserWaitTimeTrackers/BuildUserWaitTimeTracker.cs
+++ Assets/Scripts/Editor/UserWaitTimeTrackers/BuildUserWaitTimeTracker.cs
@@ -1,17 +1,27 @@
-﻿using UnityEditor.Build;
+﻿using System.Diagnostics;
+using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
 namespace UnityEditorDevelopmentBenchmark.Editor.UserWaitTimeTrackers
 {
-    public class BuildUserWaitTimeTracker : UserWaitTimeTrackerBase, IPostprocessBuildWithReport
+    public class BuildUserWaitTimeTracker : UserWaitTimeTrackerBase, IPreprocessBuildWithReport, IPostprocessBuildWithReport
     {
         protected override string FriendlyName => "Building";
 
         public int callbackOrder => 0;
+
+        private Stopwatch _stopwatch;
         
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
         public void OnPostprocessBuild(BuildReport report)
         {
-            ReportSingleWaitEvent(report.summary.totalTime);
+            // We're not using report.summary.totalTime here, because it just doesn't work.
+            _stopwatch.Stop();
+            ReportSingleWaitEvent(_stopwatch.Elapsed);
         }
     }
 }
