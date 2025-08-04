aa94315e5b6fd22a659ac2af020a52fdbab0fb03
Xericore <xericore@gmail.com>
2025-08-04 22:44:19 +0200

    Working on BenchmarkRunner.

--- Packages/com.xericore.unityeditordevelopmentbenchmark/Editor/BenchmarkRunner.cs
+++ Packages/com.xericore.unityeditordevelopmentbenchmark/Editor/BenchmarkRunner.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Reflection;
 using JetBrains.Annotations;
 using UnityEditor;
 using UnityEditorDevelopmentBenchmark.Editor.Util;
@@ -15,24 +17,126 @@ namespace UnityEditorDevelopmentBenchmark.Editor
     {
         private static Stopwatch _stopwatch;
         
+        private const float _maxLoopTimeInSeconds = 10f;
+        
+        [MenuItem("Window/Analysis/Start Benchmark")]
+        [UsedImplicitly]
         public static void StartBenchmark()
         {
-            Debug.Log("Starting benchmark...");
+            AttachCoroutineToEditorUpdate(BenchmarkCoroutine());
+        }
+
+        private static void AttachCoroutineToEditorUpdate(IEnumerator routine)
+        {
+            EditorApplication.update += Step;
+            return;
+
+            void Step()
+            {
+                if (!routine.MoveNext())
+                {
+                    EditorApplication.update -= Step;
+                }
+            }
+        }
+
+        private static IEnumerator BenchmarkCoroutine()
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                Debug.LogWarning("Cannot start benchmark while in play mode.");
+                yield break;
+            }
+
+            if(!TryDisableConsoleClearOnPlay())
+            {
+                Debug.LogWarning("Couldn't disable console clear on play. This may cause the benchmark to not show all logs. Please disable it manually in the Console window settings.");
+            }
+
+            yield return WaitForCompilation();
+
+            Debug.Log("Preparing benchmark...");
+            yield return new WaitForSeconds(1);
+
+            Debug.Log("<color=lime>Starting benchmark...</color>");
             _stopwatch = Stopwatch.StartNew();
 
             EditorApplication.EnterPlaymode();
-            
+
+            yield return WaitForPlayMode();
+
             Debug.Log("Entered play mode.");
-            
+
+            yield return new WaitForSeconds(1);
+
             EditorApplication.ExitPlaymode();
-            
+
             Debug.Log("Exited play mode.");
-            
+
             _stopwatch.Stop();
 
             var totalDuration = _stopwatch.Elapsed + EditorStartupUtil.LastStartupDuration;
-            
+
+            Debug.Log("<color=red>Finished benchmark...</color>");
             Debug.Log($"Benchmark total time: {totalDuration}");
+        }
+        
+        private static bool TryDisableConsoleClearOnPlay()
+        {
+            var assembly = typeof(EditorWindow).Assembly;
+            var consoleWindowType = assembly.GetType("UnityEditor.ConsoleWindow");
+            var field = consoleWindowType.GetField("ms_ConsoleWindow", BindingFlags.Static | BindingFlags.NonPublic);
+            if (field == null)
+            {
+                return false;
+            }
+
+            var consoleInstance = field.GetValue(null);
+            if (consoleInstance == null)
+            {
+                return false;
+            }
+
+            var clearOnPlayField =
+                consoleWindowType.GetField("m_ClearOnPlay", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (clearOnPlayField == null)
+            {
+                return false;
+            }
+
+            clearOnPlayField.SetValue(consoleInstance, false);
+            return true;
+
+        }
+
+        private static IEnumerator WaitForCompilation()
+        {
+            var startTime = (float) EditorApplication.timeSinceStartup;
+            while (EditorApplication.isCompiling)
+            {
+                if ((float) EditorApplication.timeSinceStartup - startTime > _maxLoopTimeInSeconds)
+                {
+                    Debug.LogWarning("Timeout while waiting for compilation to finish.");
+                    yield break;
+                }
+
+                yield return null;
+            }
+        }
+        
+        private static IEnumerator WaitForPlayMode()
+        {
+            var startTime = (float) EditorApplication.timeSinceStartup;
+            while (!EditorApplication.isPlaying)
+            {
+                if ((float) EditorApplication.timeSinceStartup - startTime > _maxLoopTimeInSeconds)
+                {
+                    Debug.LogWarning("Timeout while waiting for play mode to enter.");
+                    yield break;
+                }
+
+                yield return null;
+            }
         }
     }
 }
